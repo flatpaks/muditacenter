7336db447a982ad1887aba7f047b8950e8972214
adamthiede <adamthiede@users.noreply.github.com>
2025-09-25 15:29:38 +0000

    autobuild for 3.2.0

--- autobuild.py
+++ autobuild.py
@@ -15,7 +15,7 @@ def commit(version):
         subprocess.run(commit, shell=True)
         subprocess.run(tagetc, shell=True)
 
-current="3.1.0"
+current="3.2.0"
 
 # query github
 endpoint="https://api.github.com/repos/mudita/mudita-center/releases"
